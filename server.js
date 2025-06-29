9ea590ff34dd7877367d2fb2cd874941b9b452cc
WRITERR-gif <w0758025@gmail.com>
2025-06-29 21:56:38 +0300

    Update server.js

--- server.js
+++ server.js
@@ -146,5 +146,6 @@ app.get("/admin/logout", (req, res) => {
     });
 });
 
-// start
-app.listen(3000, () => console.log("Server running on http://localhost:3000"));
+// Render + local compatible:
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
